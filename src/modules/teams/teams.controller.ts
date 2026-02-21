5b2829e83b18dbf929a3c96cfa0a013e19372203
grwna <13523035@std.stei.itb.ac.id>
2026-02-21 22:26:58 +0700

    feat: add minor error handling in teams api

--- src/modules/teams/teams.controller.ts
+++ src/modules/teams/teams.controller.ts
@@ -3,10 +3,35 @@ import { insertTeamSchema } from "./teams.schema.js";
 import { createTeam } from "./teams.service.js";
 
 export const handleCreateTeam = async (c: Context) => {
-    const data = await c.req.json()
-    const parsedData = insertTeamSchema.parse(data)
+    try {
+        const data = await c.req.json()
+        const parsedData = insertTeamSchema.parse(data)
 
-    const insertedTeam = await createTeam(parsedData);
+        const insertedTeam = await createTeam(parsedData);
 
-    return c.json({success: true, data: insertedTeam}, 201)
+        return c.json({ success: true, data: insertedTeam }, 201)
+    } catch (error: any) {
+        if (error.name === "ZodError") {
+            return c.json({ success: false, error: "Validation failed", details: error.errors }, 400);
+        }
+
+        // unique contraints violation
+        if (error.code === "23505") {
+            const detail = error.detail;
+            
+            // TODO: test these includes keys
+            if (detail.includes("team_name")) {
+                return c.json({ success: false, error: "Team name already exists" }, 409);
+            }
+
+            // TODO: clarify if an account can register to multiple comp
+            if (detail.includes("members_pkey") || detail.includes("user_id")) {
+                return c.json({ success: false, error: "User is already a member of a team" }, 409);
+            }
+        }
+
+        // unexpected error
+        console.error(error);
+        return c.json({ success: false, error: "An unexpected error occurred" }, 500);
+    }
 }
--- src/modules/teams/teams.service.ts
+++ src/modules/teams/teams.service.ts
@@ -3,22 +3,28 @@ import { teams, members } from "../../db/schema.js";
 
 // works as a transaction (teams - members) -> a team cannot be created without leader
 export const createTeam = async (teamData: any) => {
-    return await db.transaction(async (tx) => {
-        const [newTeam] = await tx.insert(teams).values({
-            userId: teamData.userId,   //TODO: ensure userId means leader's auth id
-            teamName: teamData.teamName,
-            leaderName: teamData.leaderName,
-            university: teamData.university,
-            leaderMajor: teamData.leaderMajor,
-            category: teamData.category,
-        }).returning(); // return inserted values
+    try {
+        return await db.transaction(async (tx) => {
+            const [newTeam] = await tx.insert(teams).values({
+                userId: teamData.userId,   //TODO: ensure userId exists
+                teamName: teamData.teamName,
+                leaderName: teamData.leaderName,
+                university: teamData.university,
+                leaderMajor: teamData.leaderMajor,
+                category: teamData.category,
+            }).returning(); // return inserted values
 
-        await tx.insert(members).values({
-            teamId: newTeam.id,
-            fullName: teamData.leaderName,
-            major: teamData.leaderMajor,
+            // TODO: ADD USER ID CHECKS AND INSERTION
+            await tx.insert(members).values({
+                userId: teamData.userId,
+                teamId: newTeam.id,
+                fullName: teamData.leaderName,
+                major: teamData.leaderMajor,
+            })
+
+            return newTeam;
         })
-
-        return newTeam;
-    })
+    } catch (error: any) {
+        throw error;
+    }
 }
